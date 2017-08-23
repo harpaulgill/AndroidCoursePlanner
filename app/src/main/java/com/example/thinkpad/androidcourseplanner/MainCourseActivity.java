2922e0c5972267ebb9583cd44461fed6bef07f20
Thinkpad <harpaul_gill@hotmail.com>
2017-08-22 22:56:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/thinkpad/androidcourseplanner/MainCourseActivity.java
@@ -0,0 +1,13 @@
+package com.example.thinkpad.androidcourseplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_course);
+    }
+}
