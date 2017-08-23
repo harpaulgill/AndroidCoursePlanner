e0fcd3e6c83048e7f6ba75921ed100061bef2dcc
Thinkpad <harpaul_gill@hotmail.com>
2017-08-22 23:19:13 -0700

    added course and coursedb files

---
+++ app/src/main/java/com/example/thinkpad/androidcourseplanner/Course.java
@@ -0,0 +1,62 @@
+package com.example.thinkpad.androidcourseplanner;
+
+/**
+ * Created by Harpaul on 8/19/2017.
+ * This class is used to make new course objects for every entry in the json data
+ */
+
+public class Course {
+
+    private final String subject;
+    private final String courseNum;
+    private final String credits;
+    private final String faculty;
+    private final Boolean isSelected;
+    private int term;
+
+    /**
+     * Create a new Course Object with all of its attributes
+     * @param subject
+     *            the course's subject ENGL, MATH etc.
+     * @param courseId
+     *            the sourse number
+     * @param credits
+     *            the amount of credits this course is worth
+     */
+    public Course(String subject, String courseId, String credits, String faculty) {
+        this.subject = subject;
+        this.courseNum = courseId;
+        this.credits = credits;
+        this.faculty = faculty;
+        this.isSelected = false;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public String getCourseNum() {
+        return this.courseNum;
+    }
+
+    public String getCredits() {
+        return this.credits;
+    }
+
+    public String getFaculty() {
+        return this.faculty;
+    }
+
+    public Boolean isSelected() {
+        return this.isSelected;
+    }
+
+    public void setTerm(int term){
+        this.term = term;
+    }
+
+    public int getTerm(){
+        return this.term;
+    }
+
+}
---
+++ app/src/main/java/com/example/thinkpad/androidcourseplanner/CourseDB.java
@@ -0,0 +1,72 @@
+package com.example.thinkpad.androidcourseplanner;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by kgrover on 2017-07-09.
+ */
+
+public class CourseDB {
+
+    Context context;
+
+    protected CourseDB(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<Course> getAllCourses() throws IOException, JSONException {
+        // Reading text file from assets folder
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = null;
+        try {
+            AssetManager assetManager = context.getAssets();
+            InputStream is = assetManager.open("jsondata.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+            String temp;
+            while ((temp = br.readLine()) != null)
+                sb.append(temp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close(); // stop reading
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String myjsonstring = sb.toString();
+
+        // Creating JSONObject from String
+        JSONObject jsonObjMain = new JSONObject(myjsonstring);
+
+        // Creating JSONArray from JSONObject
+        JSONArray jsonArray = jsonObjMain.getJSONArray("courses");
+
+        ArrayList<Course> courses = new ArrayList<Course>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            // Creating JSONObject from JSONArray
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            String courseSubject = jsonObj.getString("courseSubject");
+            String courseId = jsonObj.getString("courseId");
+            String credits = jsonObj.getString("credits");
+            String faculty = jsonObj.getString("faculty");
+
+            Course course = new Course(courseSubject, courseId, credits, faculty);
+            courses.add(course);
+        }
+
+        return courses;
+    }
+}
